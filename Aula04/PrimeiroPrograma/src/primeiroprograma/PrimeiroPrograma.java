7803d30b514562bd32b14bde1050d281e6facebd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-27 10:22:27 +0100

    Aula04 - Primeiro programa - Olá Mundo

--- Aula04/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ Aula04/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -4,7 +4,8 @@ package primeiroprograma;
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
-      System.out.print("Olá Mundo!");
+        System.out.print("Olá Mundo!");
+        System.out.println("Estou livre da Maldição");
     }
     
 }
