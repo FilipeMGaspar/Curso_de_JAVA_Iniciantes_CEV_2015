971eadfc1f602285f23b75b15235ac4d69e51ab5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-28 10:18:26 +0100

    Aula04 - Exercicios - Resolução do Monitor

--- Aula04/ResolucaoDoEcra/src/resolucaodoecra/ResolucaoDoEcra.java
+++ Aula04/ResolucaoDoEcra/src/resolucaodoecra/ResolucaoDoEcra.java
@@ -1,11 +1,15 @@
 
 package resolucaodoecra;
 
+import java.awt.Toolkit;
+
 
 public class ResolucaoDoEcra {
 
     public static void main(String[] args) {
         // Resolução do ecrã
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        
     }
     
 }
