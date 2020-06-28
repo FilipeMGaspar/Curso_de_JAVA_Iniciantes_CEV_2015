f76b220939025955b0dc7fc4779b48600a7a14bf
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-28 10:23:01 +0100

    Aula04 - Exercicios - Idioma do sistema

--- Aula04/IdiomaDoSistema/src/idiomadosistema/IdiomaDoSistema.java
+++ Aula04/IdiomaDoSistema/src/idiomadosistema/IdiomaDoSistema.java
@@ -7,7 +7,8 @@ public class IdiomaDoSistema {
 
     public static void main(String[] args) {
         // Verificar qual é o idioma do sistema
-        Locale idioma = Locale.getDefault();
+        Locale idioma = Locale.getDefault(); // Para obter o idioma do sistema
+        System.out.print("O idioma do sistema está configurado em: ");
     }
     
 }
