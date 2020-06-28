bf9073972e60de8dc84d76b1d28804fb7eeb7891
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-28 10:09:01 +0100

    Aula04 - Exercicios - Hora do sistema

--- Aula04/Horadosistema/src/horadosistema/Horadosistema.java
+++ Aula04/Horadosistema/src/horadosistema/Horadosistema.java
@@ -7,7 +7,7 @@ public class Horadosistema {
 
     public static void main(String[] args) {
        Date relogio = new Date();//relogio é um objeto devido ao new
-        System.out.println("");
+        System.out.println("A hora do sistema é");
        
     }
     
