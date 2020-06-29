d93c9d411d9bd559f16fb667e5acbc758869d8a0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-29 20:18:04 +0100

    Aula05 - Ola Mundo Java swing e JavaFx

--- Aula05/OlaMundoJavaFx/src/olamundojavafx/FXMLDocumentController.java
+++ Aula05/OlaMundoJavaFx/src/olamundojavafx/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -20,12 +21,11 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label lblMensagem;
-    private 
+    private Button btnClick;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void clicouBotao(ActionEvent event) {
+        
     }
     
     @Override
