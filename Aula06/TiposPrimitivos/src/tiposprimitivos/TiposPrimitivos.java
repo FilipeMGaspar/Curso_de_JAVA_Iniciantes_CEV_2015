7e65fb3dc7404af60910e26aaec9a3fc5f171f34
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-07-05 12:51:12 +0100

    Aula06 - Tipos Primitivos e Manipilação de dados

--- Aula06/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ Aula06/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -19,8 +19,10 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
-        String nome = "Gustavo";
-        float nota = 8.5f;
+        System.out.print("Indique o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.println("");
+        float nota = teclado.nextFloat();
         //System.out.print("A nota é: " + nota);
         System.out.println("A nota é: " + nota);
         
