7660621cb6e7a4d6b3a38335d2c7b593aad3bb57
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-07-05 12:58:24 +0100

    Aula06 - Tipos Primitivos e Manipilação de dados

--- Aula06/TesteTipos/src/testetipos/TesteTipos.java
+++ Aula06/TesteTipos/src/testetipos/TesteTipos.java
@@ -16,6 +16,8 @@ public class TesteTipos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int idade = 30;
+        String valor = idade;
     }
     
 }
