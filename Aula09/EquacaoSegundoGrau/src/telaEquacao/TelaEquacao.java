4c7bc3614a351899d49e7b947ecd544fd5f0d050
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-08-23 17:10:19 +0100

    Aula09 - Exercicios

--- Aula09/EquacaoSegundoGrau/src/telaEquacao/TelaEquacao.java
+++ Aula09/EquacaoSegundoGrau/src/telaEquacao/TelaEquacao.java
@@ -281,6 +281,7 @@ public class TelaEquacao extends javax.swing.JFrame {
 
     private void btnCalcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalcActionPerformed
         // TODO add your handling code here:
+        
         panResultado.setVisible(true);
     }//GEN-LAST:event_btnCalcActionPerformed
 
