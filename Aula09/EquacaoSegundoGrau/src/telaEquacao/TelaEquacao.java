58c8f9cb8447fc444a1c7c5795339bd8f2a6e8e5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-08-23 16:49:27 +0100

    Aula09 - Exercicios

--- Aula09/EquacaoSegundoGrau/src/telaEquacao/TelaEquacao.java
+++ Aula09/EquacaoSegundoGrau/src/telaEquacao/TelaEquacao.java
@@ -35,7 +35,7 @@ public class TelaEquacao extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        lblA = new javax.swing.JLabel();
         lblB = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
@@ -68,8 +68,8 @@ public class TelaEquacao extends javax.swing.JFrame {
         jLabel5.setForeground(new java.awt.Color(0, 102, 204));
         jLabel5.setText("Equações de Segundo Grau");
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel6.setText("A");
+        lblA.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lblA.setText("A");
 
         lblB.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         lblB.setText("<html>B<sup>2</sup></html>");
@@ -113,7 +113,7 @@ public class TelaEquacao extends javax.swing.JFrame {
                                 .addGap(7, 7, 7)
                                 .addComponent(jLabel12)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblA, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(2, 2, 2)
                                 .addComponent(jLabel7)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -154,7 +154,7 @@ public class TelaEquacao extends javax.swing.JFrame {
                     .addComponent(lblB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel10)
-                    .addComponent(jLabel6)
+                    .addComponent(lblA)
                     .addComponent(jLabel11)
                     .addComponent(jLabel8)
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -209,10 +209,10 @@ public class TelaEquacao extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel lblA;
     private javax.swing.JLabel lblB;
     private javax.swing.JSpinner txtA;
     private javax.swing.JSpinner txtB;
