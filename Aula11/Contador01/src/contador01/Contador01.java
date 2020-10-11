f961e448a58d2723d81ea5f0cfcbe421e7b87a93
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-11 11:32:45 +0100

    Aula11 - Estruturas de repetição (Parte 1)

--- Aula11/Contador01/src/contador01/Contador01.java
+++ Aula11/Contador01/src/contador01/Contador01.java
@@ -17,7 +17,9 @@ public class Contador01 {
     public static void main(String[] args) {
         // TODO code application logic here
         int cc = 0;
-        
+        while(cc<4){
+            
+        }
     }
     
 }
