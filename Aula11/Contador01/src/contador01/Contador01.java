a2ecc9eab81a8e02fec275a68f1f2a8e47588181
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-14 14:27:45 +0100

    Aula11 - Estruturas de repetição (Parte 1)

--- Aula11/Contador01/src/contador01/Contador01.java
+++ Aula11/Contador01/src/contador01/Contador01.java
@@ -19,8 +19,8 @@ public class Contador01 {
         int cc = 0;
         while(cc<10){
             cc++;
-            if(cc == 5 || cc == 7){
-                
+            if(cc == 5 || cc == 7 || cc == 9){
+                continue;
             }
             System.out.println("Cambalhota! " + cc);
         }
