d4a81a7410c6c29de08bbb7626a9a63bdc16464c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-23 18:49:42 +0100

    Aula12 - Estruturas de Repetição (Parte 2)

--- Aula12/Repeticao01/src/repeticao01/Repeticao01.java
+++ Aula12/Repeticao01/src/repeticao01/Repeticao01.java
@@ -16,6 +16,11 @@ public class Repeticao01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int cc = 0;
+        do{
+            System.out.println("Cabalhota!");
+            cc++;
+        }while(cc < 4);
     }
     
 }
