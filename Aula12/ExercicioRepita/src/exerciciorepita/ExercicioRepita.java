6863ec1efb201457a6f64f5008a4804c6141e1ca
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-25 09:41:47 +0000

    Aula12 - Exercicios

--- Aula12/ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
+++ Aula12/ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -18,8 +18,12 @@ public class ExercicioRepita {
     public static void main(String[] args) {
         // TODO code application logic here
         //JOptionPane.showMessageDialog(null, "Olá Mundo!", "Boas Vindas", JOptionPane.ERROR_MESSAGE);
-       int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número"));
-       JOptionPane.showMessageDialog(null,"Você digitou o valor "+ n);
+       //int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número"));
+       //JOptionPane.showMessageDialog(null,"Você digitou o valor "+ n);
+       int n;
+       do{
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "<html>Digite um número <br> <strong>0</strong> <em>para parar!</em></html>"));
+       }while(n != 0);
     }
     
 }
