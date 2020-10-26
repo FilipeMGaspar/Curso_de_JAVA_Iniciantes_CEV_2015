7342c3be6befaba31e3fcf02e9e02f9c9d9d5140
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-26 15:07:03 +0000

    Aula13 - Estruturas de Repetição (Parte 3)

--- Aula13/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
+++ Aula13/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -19,6 +19,10 @@ public class RepeticaoFor {
         for(int cc=1; cc<=4; cc++){
             System.out.println("Cambalhota "+cc+"!");
         }
+        
+        for(int cc=15; cc>=5; cc-=2){
+            System.out.println(cc);
+        }
     }
     
 }
