c6c7f7b3f4a1540d45aa5583b364d6832ac4da69
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-30 14:26:08 +0000

    Aula14 - Vetores (Vetores)

--- Aula14/Vetor01/src/vetor01/Vetor01.java
+++ Aula14/Vetor01/src/vetor01/Vetor01.java
@@ -16,7 +16,7 @@ public class Vetor01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n[] = new int[4];
+        int n[] = {3, 2, 8, 7, 7, 5, 4};
     }
     
 }
