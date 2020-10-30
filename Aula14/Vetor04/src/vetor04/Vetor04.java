ccb657e12a9d043a424480fe78b737a8c2127fb2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-30 14:55:46 +0000

    Aula14 - Vetores (Vetores)

--- Aula14/Vetor04/src/vetor04/Vetor04.java
+++ Aula14/Vetor04/src/vetor04/Vetor04.java
@@ -19,6 +19,10 @@ public class Vetor04 {
     public static void main(String[] args) {
         // TODO code application logic here
         int vet[] = {3, 7, 1, 9, 4, 2};
+        for(int valor: vet){
+            System.out.println(valor);
+        }
+        System.out.println("");
         int p = Arrays.binarySearch(vet, 1);// pesquisa no vetor "vet" pelo valor 1
         System.out.println("Encontrei o valor na posição: " + p);
     }
