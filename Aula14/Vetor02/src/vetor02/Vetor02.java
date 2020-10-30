93f7c0e4f89cf43379f1a7f7a38e3628546eb6e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-10-30 14:44:14 +0000

    Aula14 - Vetores (Vetores)

--- Aula14/Vetor02/src/vetor02/Vetor02.java
+++ Aula14/Vetor02/src/vetor02/Vetor02.java
@@ -24,6 +24,5 @@ public class Vetor02 {
         for(int i=0; i<mes.length; i++){
             System.out.println("O mês de "+ mes[i]+ " ,tem "+ tot[i]+" dias!" );
         }
-    }
-    
+    }    
 }
