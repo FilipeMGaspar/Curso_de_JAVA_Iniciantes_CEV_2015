f7b9bd38099d3b6b4df5c0f38743fd852d14a5ff
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-11-06 17:01:24 +0000

    Aula15 - Métodos

--- Aula15/Testefuncao03/src/testefuncao03/Testefuncao03.java
+++ Aula15/Testefuncao03/src/testefuncao03/Testefuncao03.java
@@ -17,6 +17,7 @@ public class Testefuncao03 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Vai começar a contagem");
+        System.out.println(Operacoes.contador(1,10));
     }
     
 }
