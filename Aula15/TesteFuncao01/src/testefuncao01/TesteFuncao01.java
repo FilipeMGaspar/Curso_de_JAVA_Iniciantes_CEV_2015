7ea434139d70f99c55faa675b875c714bbae212d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-11-06 16:36:24 +0000

    Aula15 - Métodos

--- Aula15/TesteFuncao01/src/testefuncao01/TesteFuncao01.java
+++ Aula15/TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -15,14 +15,14 @@ public class TesteFuncao01 {
      * @param args the command line arguments
      */
     
-    void soma(int a, int b){
+    static void soma(int a, int b){
         int soma = a+ b;
-        System.out.println("A soma de "+ a +" com "+ b + "Tem o valor de "+soma);
+        System.out.println("A soma de "+ a +" com "+ b + " tem o valor de "+soma);
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        soma(5, 2);
     }
     
 }
